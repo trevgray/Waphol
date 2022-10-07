6d8655b09572ce16eedbac7dac6ce01cd734dae9
Trev <daeteri360@gmail.com>
2022-10-06 20:21:13 -0400

    Client and Server V1 uploaded

--- ComponentFramework22.4.0/ComponentFramework/NetworkManager.cpp
+++ ComponentFramework22.4.0/ComponentFramework/NetworkManager.cpp
@@ -24,6 +24,41 @@ NetworkManager::NetworkManager() {
 
 NetworkManager::~NetworkManager() {
 	std::cout << "DESTORYTING\n";
+	if (networkMode == Offline) { //we are not using networking - do nothing
+		return;
+	}
+	else if (networkMode == Server) {
+		//shutdown the connection on our end
+		iResult = shutdown(clientSocket, SD_SEND);
+		if (iResult == SOCKET_ERROR) {
+			std::cout << "Shutdown failed with error: " << WSAGetLastError() << std::endl;
+			closesocket(listenSocket);
+			WSACleanup();
+			system("pause");
+			return;
+		}
+
+		std::cout << "Connection closed" << std::endl;
+
+		closesocket(clientSocket);
+	}
+	else if (networkMode == Client) {
+		//shutdown the connection
+		iResult = shutdown(connectSocket, SD_SEND);
+		if (iResult == SOCKET_ERROR) {
+			std::cout << "Shutdown failed with error: " << WSAGetLastError() << std::endl;
+			closesocket(connectSocket);
+			WSACleanup();
+			system("pause");
+			return;
+		}
+
+		std::cout << "Connection closed" << std::endl;
+
+		closesocket(connectSocket);
+	}
+	
+	WSACleanup();
 }
 
 void NetworkManager::Run() {
@@ -31,7 +66,71 @@ void NetworkManager::Run() {
 		if (networkMode == Offline) { //we are not using networking - do nothing
 			return;
 		}
-		std::cout << EngineManager::Instance()->GetTimer()->GetCurrentTicks() << std::endl;
+		else if(networkMode == Server) {
+			//listen
+			iResult = listen(listenSocket, SOMAXCONN); //SOMAXCONN allows maximum number of connections
+			if (iResult == SOCKET_ERROR) {
+				std::cout << "Listen Socket failed with error: " << WSAGetLastError() << std::endl;
+				closesocket(listenSocket);
+				WSACleanup();
+				system("pause");
+				return;
+			}
+
+			std::cout << "Waiting for connection request..." << std::endl;
+
+			//Accept a client socket
+			clientSocket = accept(listenSocket, nullptr, nullptr);
+			if (clientSocket == INVALID_SOCKET) {
+				std::cout << "Accept failed with error: " << WSAGetLastError() << std::endl;
+				closesocket(listenSocket);
+				WSACleanup();
+				system("pause");
+				return;
+			}
+
+			std::cout << "Connected to client" << std::endl;
+
+			closesocket(listenSocket);
+
+			//receive until the peer shutdown the connect
+			for (int x = 0; x < DEFAULT_BUFFER_LENGTH; x++) {
+				recvbuf[x] = 0;
+			}
+			iResult = recv(clientSocket, recvbuf, DEFAULT_BUFFER_LENGTH, 0);
+			if (iResult > 0) {
+				std::cout << "Received string: " << recvbuf << std::endl;
+			}
+			else if (iResult == 0) {
+				std::cout << "Connection closing..." << std::endl;
+				break;
+			}
+			else {
+				std::cout << "Receive failed with error: " << WSAGetLastError() << std::endl;
+				closesocket(listenSocket);
+				WSACleanup();
+				system("pause");
+				return;
+			}
+		}
+		else if (networkMode == Client) {
+			//Receive until the peer closes connection
+			std::string message;
+			message.clear();
+			std::cout << "Input a sting to send (!exit to disconnect): ";
+			std::cin >> message;
+			int messageSize = strlen(message.c_str());
+			//Send an initial buffer
+			iResult = send(connectSocket, message.c_str(), messageSize, 0);
+			if (iResult == SOCKET_ERROR) {
+				std::cout << "Send failed with error: " << iResult << std::endl;
+				closesocket(connectSocket);
+				WSACleanup();
+				system("pause");
+				return;
+			}
+			std::cout << "Bytes sent: " << iResult << std::endl;
+		}
 	}
 }
 
@@ -49,8 +148,8 @@ bool NetworkManager::Initialize(NetworkNode networkMode_) {
 		return false;
 	}
 
-	if (networkMode == Server) { //set up the instance as a server
-		//set up hints
+	else if (networkMode == Server) { //set up the instance as a server
+		//hints define the connection type and the address that will be used
 		ZeroMemory(&hints, sizeof(hints));
 		hints.ai_family = AF_INET;
 		hints.ai_socktype = SOCK_STREAM; //TCP works like a stream
@@ -88,6 +187,41 @@ bool NetworkManager::Initialize(NetworkNode networkMode_) {
 		}
 		freeaddrinfo(result);
 	}
+	else if (networkMode == Client) {
+		//hints define the connection type and the address that will be used
+		ZeroMemory(&hints, sizeof(hints));
+		hints.ai_family = AF_UNSPEC; //don't specificity the address family (IVP6 / IVP 4)
+		hints.ai_socktype = SOCK_STREAM; //TCP works like a stream
+		hints.ai_protocol = IPPROTO_TCP;
 
+		for (ptr = result; ptr != NULL; ptr = ptr->ai_next) { //move addresses till one succeeds
+			//Create a socket for connecting to the server
+			connectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol); //find a open socket
+			if (connectSocket == INVALID_SOCKET) {
+				std::cout << "Socket failed with error: " << iResult << std::endl;
+				WSACleanup();
+				system("pause");
+				return 1;
+			}
+
+			//Connect to the server
+			iResult = connect(connectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+			if (iResult == SOCKET_ERROR) {
+				closesocket(connectSocket);
+				connectSocket = INVALID_SOCKET;
+				continue;
+			}
+			std::cout << "Connected to server" << std::endl;
+			break;
+		}
+
+		freeaddrinfo(result);
+		if (connectSocket == INVALID_SOCKET) {
+			std::cout << "Unable to connect to the server: " << iResult << std::endl;
+			WSACleanup();
+			system("pause");
+			return 1;
+		}
+	}
 	return true;
 }
